d3acaaf15b26bd9699dc940f9dbc4267b44e3542
DeadCatLady <cww.christa@gmail.com>
2018-11-19 15:49:11 -0800

    Updates search bar to work

--- templates/block--block--2.tpl.php
+++ templates/block--block--2.tpl.php
@@ -2,12 +2,12 @@
   <?php if (isset($title_suffix['contextual_links'])): ?>
     <?php print render($title_suffix['contextual_links']); ?>
   <?php endif; ?>
-  <div class="content">
-    <form class="search-bar form-search" _lpchecked="1" onSubmit="return search();">
-      <input id="search" class="search-input" type="text">
+  <div class="content">i
+    <form id="search-form" class="search-bar form-search" _lpchecked="1" onSubmit="return false;">
+      <input name="search" class="search-input" type="text">
       <label for="search" class="show-large">Search by Title/ISBN/Author/Keyword</label>
       <label for="search" class="show-small">Search . . .</label>
-      <button class="search-button">
+      <button type="submit" class="search-button">
         <i class="icon-search icon-large icon-white"><wbr></i>
       </button>
     </form>
